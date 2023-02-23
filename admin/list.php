00d1a453802afa145c6c10af1f6a2891100b1ef6
chirogod <rolandelli_augus@hotmail.com>
2023-02-23 11:51:39 -0300

    Detail

--- admin/list.php
+++ admin/list.php
@@ -1,24 +1,6 @@
 <?php
 include('conexion.php');
-/*
-function obtenerProductos(){
-    include('conexion.php');
-    $query = "SELECT p.id, p.name from product p ";
-    $result = mysqli_query($conn, $query);
-    return $result;
-}
 
-$result = obtenerProductos();
-
-function obtenerUsers(){
-    include('conexion.php');
-    $query = "SELECT u.username from user u";
-    $result_user = mysqli_query($conn, $query);
-    return $result_user;
-}
-
-$result_user = obtenerUsers();
-*/
 
 $sql = $conn->query("SELECT p.id, u.username, b.brand, p.name from user u left join product p on u.id = p.created_by left join brand b on p.created_by = b.id");
 while ($row = $sql->fetch_array()) {
@@ -32,7 +14,7 @@ while ($row = $sql->fetch_array()) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LIST</title>
 </head>
 <body>
 
--- index.php
+++ index.php
@@ -1,13 +1,48 @@
+<!--- 
+git status // para ver si registro los cambio
+git add . // para agregar los archivos
+git commit -m "mensaje" // mensaje del commit
+git push -u origin main // PARA SUBIR LOS CAMBIOS
+git log // para ver cambios realizados en el proyecto
+
+--->
+
+<?php
+    include('admin/conexion.php');
+
+
+
+    $product = mysqli_query($conn,"SELECT p.name, p.precio, b.brand from product p left join brand b on b.id = p.brand_id ");
+
+
+
+?>                                                         
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TEST</title>
+    <title>PUNTO CELULAR</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+    <div class="producto">
+        <?php while ($resultado = mysqli_fetch_assoc($product)) : ?>
+            <div class="prod">
+                <p>producto:   <?php echo $resultado['name']?></p>
+                <p>precio:   <?php echo $resultado['precio']?></p>
+                <p>marca:   <?php echo $resultado['brand']?></p>
+            </div>
+        <?php endwhile ?>
+    </div>
+
+    <div>
+        <h1>gOLA</h1>
+    </div>
 
 
 
